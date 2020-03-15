658478d7dce1298300d01ccde1888d6f1bf300d8
leipeng <leipeng@126.com>
2020-03-15 22:00:11 +0800

    完成用户头像上传功能

--- public/assets/js/user.js
+++ public/assets/js/user.js
@@ -12,3 +12,22 @@ function render() {
     let html = template('userTpl', { data: userArr });
     $('tbody').html(html);
 };
+
+//上传图片功能
+$('#avatar').on('change', function () {
+    let formData = new FormData();
+    formData.append('avatar', this.files[0]);
+    $.ajax({
+        //对应接口文档
+        type: 'post',
+        url: '/upload',
+        data: formData,
+        // 只要是通过jquery中的ajax来实现文件上传的功能 就需要设置下面两个属性
+        processData: false,
+        contentType: false,
+        success: function (res) {
+            $('#previewImg').attr('src', res[0].avatar);
+            $('#hidden').val(res[0].avatar)
+        }
+    })
+});
